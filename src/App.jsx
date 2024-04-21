060466fec44e4752e929b175fc7cee3d7db9aaa5
Himanshu Mahto <k.himanshu2002@gmail.com>
2024-04-21 16:07:23 +0530

    created mdEditor

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import { MarkDown } from './components/markdown'
+
+function App() {
+  return (
+    <>
+    <MarkDown/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/markdown.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import "./markdown.css";
+import ReactMarkdown from "react-markdown";
+
+export const MarkDown = () => {
+  const [markdown, setmarkdown] = useState("");
+
+  useEffect(() => {
+    const savedMarkdown = localStorage.getItem("markdown");
+    if (savedMarkdown) {
+      setmarkdown(savedMarkdown);
+    }
+  }, []);
+
+  const handleChange = (e) => {
+    const value = e.target.value;
+    setmarkdown(value);
+    localStorage.setItem("markdown", value);
+  };
+
+  return (
+    <>
+      <div className="pageTitle">Markdown Editor</div>
+      <div className="container">
+        <div className="title">
+          <div className="markdown">Markdown</div>
+          <textarea
+            className="noteArea"
+            value={markdown}
+            onChange={handleChange}
+          ></textarea>
+        </div>
+        <div className="preview_title">
+          <div className="preview">Preview</div>
+          <div className="markArea">
+            <ReactMarkdown>{markdown}</ReactMarkdown>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
